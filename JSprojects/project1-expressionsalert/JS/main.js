086a3929de2b20d5c0640c1fcdf7b452344ba743
aroysss <100801481+aroysss@users.noreply.github.com>
2022-03-22 15:25:44 -0600

    js1

---
+++ JSprojects/project1-expressionsalert/JS/main.js
@@ -0,0 +1,12 @@
+
+	alert('Hello World');
+	var a= 10;
+	var b= 20;
+	var c= 30;
+	var amount= a+b+c;
+	document.write(amount);
+	var d="Hello";
+	var e="World";
+	var f= d+e; // concatenate//
+	d.fontcolor("red"); //might work in some browsers
+	document.getElementById("demo").innerHTML= f;

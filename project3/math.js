6d293ae5baca75c0669cc318a318d3e7f4759f50
aroysss <100801481+aroysss@users.noreply.github.com>
2022-03-23 16:25:45 -0600

    project3

---
+++ project3/math.js
@@ -0,0 +1,19 @@
+function (add) {
+	var a=8+8;
+	document.getElementById("add").innerHTML="8+8="+addition;
+}
+function (sub) {
+	var b=10-1;
+	document.getElementById("sub").innerHTML="10-1"+subtraction;
+}
+function mul {
+	var c=5*5;
+	document.getElementById("mul").innerHTML="5*5"+multiply;
+}
+function mod {
+	var modulus=10%3;
+	document.getElementById("mod").innerHTML="10%3"+modulus;
+}
+//math random shows random 0-1//
+
+		document.getElementById("random").innerHTML= math.random();

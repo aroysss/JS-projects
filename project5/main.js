6c517f7a1cab24ad23cac580e16af478145258f3
aroysss <100801481+aroysss@users.noreply.github.com>
2022-03-24 13:39:22 -0600

    js

---
+++ project5/main.js
@@ -0,0 +1,15 @@
+function not_function() {
+	document.getElementById("Not").innerHTML=!(29<30);
+}
+// add string and number//
+function name_function() {
+var str="name";
+var num= 5;
+str += num;
+document.getElementById("name").innerHTML=str +=num;
+}
+// 15 is equal to 15: true//
+document.write(15==15);
+document.write(20===20);
+document.write(10>5 && 50>10);
+document.write(5>10 || 10>50);
